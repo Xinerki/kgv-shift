267d56d6d0296d12af04c20776bd34b4ff1b95e4
Xinerki <simasadves@gmail.com>
2019-10-03 21:49:17 +0300

    cleanup praying i didnt break everything

--- client.lua
+++ client.lua
@@ -1,11 +1,10 @@
 
 freecamEnabled = false
-
-chatInputActive = false
-SetTimeScale(1.0)
-SetGamePaused(false)
-
 screenFX = "SwitchShortTrevorMid"
+
+originalPed = PlayerPedId()
+originalCar = 0
+originalSelfRegistered = false
 
 function toggleFreecam()
 	Citizen.CreateThread(function()
@@ -15,38 +14,22 @@ function toggleFreecam()
 			exports['shift-freecam']:SetActive(true)
 			StartScreenEffect(screenFX, 500, false)
 			PlaySound(-1, "slow", "SHORT_PLAYER_SWITCH_SOUND_SET", 0, 0, 1)
-			pos = GetGameplayCamCoord()
-			pos = vector3(pos.x, pos.y, pos.z+1.0)
-			local rotx, roty, rotz = table.unpack(GetEntityRotation(PlayerPedId()))
-			rotation = vector3(rotz+GetGameplayCamRelativeHeading(), GetGameplayCamRelativePitch()-10.0, 0.0)
-			fov = 50.0
-			roll = 0.0
-			-- DisableAllControlActions(0)
 			SetPlayerControl(PlayerId(), false, 0)
 			TaskVehicleDriveWander(PlayerPedId(), GetVehiclePedIsIn(PlayerPedId(), false), 10.0, 0)
-			-- SetTimeScale(0.0)
 			
 			local vehicle = GetVehiclePedIsIn(PlayerPedId())
-			
 			UseParticleFxAssetNextCall('core')
 			trail1 = StartParticleFxLoopedOnEntityBone('veh_light_red_trail', vehicle, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, GetEntityBoneIndexByName(vehicle, "taillight_l"), 1.0, false, false, false)
-			SetParticleFxLoopedEvolution(trail1, "speed", 1.0, false);
-			
+			SetParticleFxLoopedEvolution(trail1, "speed", 1.0, false)
 			UseParticleFxAssetNextCall('core')
 			trail2 = StartParticleFxLoopedOnEntityBone('veh_light_red_trail', vehicle, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, GetEntityBoneIndexByName(vehicle, "taillight_r"), 1.0, false, false, false)
-			SetParticleFxLoopedEvolution(trail2, "speed", 1.0, false);
+			SetParticleFxLoopedEvolution(trail2, "speed", 1.0, false)
 		else
 			exports['shift-freecam']:SetActive(false)
 			StartScreenEffect(screenFX, 500, false)
-			-- StopScreenEffect(screenFX)
 			PlaySound(-1, "slow", "SHORT_PLAYER_SWITCH_SOUND_SET", 0, 0, 1)
-			-- EnableAllControlActions(0)
 			SetPlayerControl(PlayerId(), true, 0)
 			ClearPedTasks(PlayerPedId())
-			UnlockMinimapPosition()
-			UnlockMinimapAngle(0.0)
-			-- SetTimeScale(1.0)
-				
 			StopParticleFxLooped(trail1, 0)
 			StopParticleFxLooped(trail2, 0)
 		end
@@ -61,29 +44,15 @@ Citizen.CreateThread(function()
 		if IsDisabledControlJustPressed(1, 73) then
 			toggleFreecam()
 		end
-		
-		-- SetGamePaused(freecamEnabled)
-
-		Citizen.Wait(1)
+		Citizen.Wait(0)
 	end
 end)
 
-fov = 50.0
-roll = 0.0
-
-originalPed = PlayerPedId()
-originalCar = 0
-originalSelfRegistered = false
-
-function processFreecam()
+Citizen.CreateThread(function ()
 	while true do 
 		if freecamEnabled == true then
-			
-			-- DisplaySniperScopeThisFrame()
-			
 			local resX, resY = GetScreenActiveResolution()
 			DrawRect(0.5, 0.5, 0.01, 0.01, 240, 151, 63, 150)
-			
 			
 			if IsDisabledControlJustPressed(0, 23) and originalSelfRegistered == true then
 				lastPed = PlayerPedId()
@@ -165,12 +134,8 @@ function processFreecam()
 						end
 					end
 				end
-				
 			end
 		end
-		
 		Citizen.Wait(0)	
 	end
-end
-
-Citizen.CreateThread(processFreecam)
+end)
