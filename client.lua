68a9daf2966f56336e7d2bccb9a3291aa1faf483
Xinerki <simasadves@gmail.com>
2019-10-03 21:39:58 +0300

    separate camera into a resource this shit cursed

--- client.lua
+++ client.lua
@@ -1,20 +1,6 @@
 
 freecamEnabled = false
 
-function init()
-	view1=CreateCam("DEFAULT_SCRIPTED_CAMERA", 1)
-	SetCamCoord(view1, 10.5, 13.6, 75.3)
-	SetCamRot(view1, 0.0, 0.0, 180.0)
-	SetCamFov(view1, 20.0)
-	pos = vector3(1.0, 1.0, 1.0)
-	rotation = vector3(0.0, 0.0, 0.0)
-end
-
-_SetCamRot = SetCamRot
-function SetCamRot(v, x, y, z)
-    _SetCamRot(v, y, z, x)
-end
-
 chatInputActive = false
 SetTimeScale(1.0)
 SetGamePaused(false)
@@ -26,7 +12,7 @@ function toggleFreecam()
 		freecamEnabled = not freecamEnabled
 
 		if freecamEnabled == true then
-			RenderScriptCams(true, 1, 1000,  true,  true)
+			exports['shift-freecam']:SetActive(true)
 			StartScreenEffect(screenFX, 500, false)
 			PlaySound(-1, "slow", "SHORT_PLAYER_SWITCH_SOUND_SET", 0, 0, 1)
 			pos = GetGameplayCamCoord()
@@ -50,7 +36,7 @@ function toggleFreecam()
 			trail2 = StartParticleFxLoopedOnEntityBone('veh_light_red_trail', vehicle, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, GetEntityBoneIndexByName(vehicle, "taillight_r"), 1.0, false, false, false)
 			SetParticleFxLoopedEvolution(trail2, "speed", 1.0, false);
 		else
-			RenderScriptCams(false, 1, 1000,  true,  true)
+			exports['shift-freecam']:SetActive(false)
 			StartScreenEffect(screenFX, 500, false)
 			-- StopScreenEffect(screenFX)
 			PlaySound(-1, "slow", "SHORT_PLAYER_SWITCH_SOUND_SET", 0, 0, 1)
@@ -92,39 +78,6 @@ originalSelfRegistered = false
 function processFreecam()
 	while true do 
 		if freecamEnabled == true then
-			local mouseX = GetDisabledControlNormal(0, 1) * 5 *(fov / 50.0)
-			local mouseY = GetDisabledControlNormal(0, 2) * 5 *(fov / 50.0)
-			rotation = vector3(rotation.x-mouseX, rotation.y-mouseY, 0)
-			
-			-- local ForwardMult = (rotation.y+90) / 180
-			
-			-- if ForwardMult < 0.0 then ForwardMult = 0.0 end
-			-- if ForwardMult > 1.0 then ForwardMult = 1.0 end
-		
-			local ForwardControl = -GetDisabledControlNormal(0, 31)
-			ForwardX, ForwardY = table.unpack(translateAngle(pos.x, pos.y, math.rad(-rotation.x), ForwardControl*(fov / 50.0)))
-			
-			local LeftRightControl = -GetDisabledControlNormal(0, 30)
-			LeftRightX, LeftRightY = table.unpack(translateAngle(pos.x, pos.y, math.rad(-rotation.x+90.0), LeftRightControl*(fov / 50.0)))
-			
-			local UpDownControl = GetDisabledControlNormal(0, 31)
-			UpDownX, UpDownZ = table.unpack(translateAngle(pos.y, pos.z, math.rad(-rotation.y-90.0), UpDownControl*(fov / 50.0)))
-			
-			pos = vector3(pos.x + ForwardX - LeftRightX, pos.y + ForwardY - LeftRightY, UpDownZ)
-			
-			LockMinimapPosition(pos.x, pos.y)
-			LockMinimapAngle(math.abs(math.floor(rotation.x) % 360))
-			
-			HideHudComponentThisFrame(7)
-			HideHudComponentThisFrame(8)
-			HideHudComponentThisFrame(9)
-			HideHudComponentThisFrame(6)
-			HideHudComponentThisFrame(19)
-			HideHudAndRadarThisFrame()
-			
-			SetCamCoord(view1, pos.x, pos.y, pos.z)
-			SetCamRot(view1, rotation.x, rotation.y, roll)
-			SetCamFov(view1, fov)
 			
 			-- DisplaySniperScopeThisFrame()
 			
@@ -134,7 +87,9 @@ function processFreecam()
 			
 			if IsDisabledControlJustPressed(0, 23) and originalSelfRegistered == true then
 				lastPed = PlayerPedId()
-				if originalPed == lastPed then toggleFreecam() else
+				if originalPed == lastPed then 
+					toggleFreecam() 
+				else
 					if IsPedInAnyVehicle(lastPed, false) == 1 then
 						inVehicle = true
 						vehicle = GetVehiclePedIsIn(lastPed, false)
@@ -157,9 +112,8 @@ function processFreecam()
 			end
 			
 			if IsDisabledControlJustPressed(0, 69) then
-			
-				local posX, posY, posZ = table.unpack(GetCamCoord(view1))
-				local rotX, rotY, rotZ = table.unpack(GetCamRot(view1,0))
+				local posX, posY, posZ = table.unpack(exports['shift-freecam']:GetPosition())
+				local rotX, rotY, rotZ = table.unpack(exports['shift-freecam']:GetRotation())
 				local rotZ1, rotX1 = -rotZ, rotX
 				local offsetX, offsetY, offsetZ = getCameraPositionOffset(rotZ1, rotX1, 50.0)
 				local ray = StartShapeTestRay(posX, posY, posZ, posX+offsetX, posY+offsetY, posZ+offsetZ, -1, PlayerPedId(), 0)
@@ -220,4 +174,3 @@ function processFreecam()
 end
 
 Citizen.CreateThread(processFreecam)
-Citizen.CreateThread(init)
